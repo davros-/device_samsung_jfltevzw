3e2030ca555de25985a3b99dc8be182301e71fff
davros <davros@droidconcepts.com>
2013-06-13 01:31:39 -0400

    fixed

--- droid_concepts.mk
+++ droid_concepts.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 
 # Inherit common DEMENTED makefiles.
-$(call inherit-product, vendor/DEMENTED/config/common_cdma.mk)
-$(call inherit-product, vendor/DEMENTED/config/common_phone.mk)
+$(call inherit-product, vendor/Droid_Concepts/config/common_cdma.mk)
+$(call inherit-product, vendor/Droid_Concepts/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
 
-PRODUCT_NAME := demented_jfltevzw
+PRODUCT_NAME := droid_concepts_jfltevzw
 PRODUCT_DEVICE := jfltevzw
 
